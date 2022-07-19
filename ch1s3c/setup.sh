f02cc809f0dfaf80048bd48d08613ae388ca2a2d
r4yX <108579252+r4yX@users.noreply.github.com>
2022-07-19 06:37:18 -0300

    Delete ch1s3c directory

--- ch1s3c/setup.sh
+++
@@ -1,6 +0,0 @@
-python3.7 /usr/bin/starter.py && mate-session-properties
-rm $1/main.sh
-rm $1/setup.sh
-rm $1/starter.py
-rm $1/hb.py
-sudo rm -r $1/../hb
--- ch1s3c/starter.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3.7
-
-import time as tm
-import pyautogui as pg
-import os 
-
-pg.write('mate\session\properties')
-pg.press('enter')
